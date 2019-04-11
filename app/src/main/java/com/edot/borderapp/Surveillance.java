dff4f951b66830b60a00040f418f816a3b55fcbc
RajaGopalPalaraman <44300372+RajaGopalPalaraman@users.noreply.github.com>
2019-04-12 02:36:11 +0530

    Update Surveillance.java

--- app/src/main/java/com/edot/borderapp/Surveillance.java
+++ app/src/main/java/com/edot/borderapp/Surveillance.java
@@ -1,10 +1,15 @@
 package com.edot.borderapp;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
@@ -12,6 +17,7 @@ import android.os.Message;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringDef;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
@@ -27,35 +33,77 @@ import static java.lang.Math.abs;
  * Created by Admin on 1/22/2017.
  */
 
-public class Surveillance extends Service {
-    MyLocation myLocation;
-    Location location;
-    double max_lat=181,min_lat=181,max_lon=181,min_lon=181;
-    File file,temp;
-    FileInputStream stream;
-    ByteArrayOutputStream byteArrayOutputStream;
-    int trig;
-    boolean first=true;
-    NotificationManager manager;
-    Notification notification,notification1,notification2;
-    Intent intent; int flags; int startId;
+public class Surveillance extends Service implements LocationListener {
+
+    private static final String CHANNEL_ID = "Pdjfhkj#jfkjdabf";
+
+    private static final String AT_BORDER = "At Border";
+    private static final String WITH_IN_BORDER = "With in Border";
+    private static final String OUT_OF_BORDER = "Out of Border";
+    private static final String TURN_ON_LOCATION = "Turn On GPS";
+    private static final String CONNECTING = "Connecting";
+
+    private static final int NOTIFICATION_ID = 1;
+
+    private NotificationManager notificationManager;
+
+    private NotificationCompat.Builder builder;
+
+    private double max_lat=181,min_lat=181,max_lon=181,min_lon=181;
+
+    private boolean equals(Location myLocation) {
+        if(abs(myLocation.getLatitude()-max_lat)<=0.005)
+            return true;
+        if(abs(myLocation.getLatitude()-min_lat)<=0.005)
+            return true;
+        if(abs(myLocation.getLongitude()-max_lon)<=0.005)
+            return true;
+        if(abs(myLocation.getLongitude()-min_lon)<=0.005)
+            return true;
+        return false;
+    }
+
+    private boolean out_of_border(Location myLocation) {
+        if(myLocation.getLatitude()>max_lat)
+            return true;
+        if(myLocation.getLatitude()<min_lat)
+            return true;
+        if(myLocation.getLongitude()>max_lon)
+            return true;
+        if(myLocation.getLongitude()<min_lon)
+            return true;
+        return false;
+    }
+
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        this.intent=intent;
-        this.flags=flags;
-        this.startId=startId;
-        manager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        notification1=new Notification.Builder(this).setContentTitle(getResources().getString(R.string.app_name)).setContentText("Within the border").setSmallIcon(R.drawable.stop).build();
-        notification=new Notification.Builder(this).setContentTitle(getResources().getString(R.string.app_name)).setContentText("Out of the border").setSmallIcon(R.drawable.stop).build();
-        notification2=new Notification.Builder(this).setContentTitle(getResources().getString(R.string.app_name)).setContentText("At the border").setSmallIcon(R.drawable.stop).build();
-        manager.notify(0,notification1);
-        myLocation=new MyLocation(this);
-        file=new File(Environment.getExternalStorageDirectory(),getResources().getString(R.string.app_name));
-        file=new File(file,getResources().getString(R.string.b));
-        temp=new File(file,getResources().getString(R.string.max_lat));
+    public void onCreate() {
+        super.onCreate();
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "NotiChannel";
+            String description = "NotiDesc";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.stop)
+                .setContentTitle(getResources().getString(R.string.app_name))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(false);
+
+        File file=new File(Environment.getExternalStorageDirectory(),getResources().getString(R.string.app_name));
+        file = new File(file,getResources().getString(R.string.b));
+        File temp=new File(file,getResources().getString(R.string.max_lat));
+        int trig;
+        FileInputStream stream;
         try {
             stream=new FileInputStream(temp);
-            byteArrayOutputStream=new ByteArrayOutputStream();
+            ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
             while((trig=stream.read())>0)
                 byteArrayOutputStream.write(trig);
             max_lat= Double.parseDouble(new String(byteArrayOutputStream.toByteArray()));
@@ -85,61 +133,33 @@ public class Surveillance extends Service {
             min_lon= Double.parseDouble(new String(byteArrayOutputStream.toByteArray()));
             byteArrayOutputStream.close();
             stream.close();
-        }catch (IOException e) {
-
+        }catch (IOException ignored) {
         }
         Toast.makeText(this,String.valueOf(max_lat),Toast.LENGTH_SHORT).show();
         Toast.makeText(this,String.valueOf(min_lat),Toast.LENGTH_SHORT).show();
         Toast.makeText(this,String.valueOf(max_lon),Toast.LENGTH_SHORT).show();
         Toast.makeText(this,String.valueOf(min_lon),Toast.LENGTH_SHORT).show();
-        while(true) {
-            if (equals()) {
-                if(first) {
-                    manager.cancelAll();
-                    manager.notify(5, notification2);
-                    first=false;
-                }
-            }
-            else if (out_of_border()) {
-                manager.cancelAll();
-                manager.notify(10, notification);
-                break;
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            if (equals(location)) {
+                builder.setContentText(AT_BORDER);
+            } else if (out_of_border(location)) {
+                builder.setContentText(OUT_OF_BORDER);
             }
             else {
-                manager.cancelAll();
-                manager.notify(0, notification1);
+                builder.setContentText(WITH_IN_BORDER);
             }
         }
-        return START_STICKY;
-    }
-
-    private boolean equals() {
-        if(abs(myLocation.getLocation().getLatitude()-max_lat)<=0.005)
-            return true;
-        if(abs(myLocation.getLocation().getLatitude()-min_lat)<=0.005)
-            return true;
-        if(abs(myLocation.getLocation().getLongitude()-max_lon)<=0.005)
-            return true;
-        if(abs(myLocation.getLocation().getLongitude()-min_lon)<=0.005)
-            return true;
-        return false;
-    }
-
-    private boolean out_of_border() {
-        if(myLocation.getLocation().getLatitude()>max_lat)
-            return true;
-        if(myLocation.getLocation().getLatitude()<min_lat)
-            return true;
-        if(myLocation.getLocation().getLongitude()>max_lon)
-            return true;
-        if(myLocation.getLocation().getLongitude()<min_lon)
-            return true;
-        return false;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
+        else {
+            builder.setContentText(CONNECTING);
+        }
+        startForeground(NOTIFICATION_ID,builder.build());
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,
+                0,this);
     }
     @Nullable
     @Override
@@ -150,6 +170,55 @@ public class Surveillance extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        //onStartCommand(intent,flags,startId);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(this);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (equals(location)) {
+            builder.setContentText(AT_BORDER);
+        } else if (out_of_border(location)) {
+            builder.setContentText(OUT_OF_BORDER);
+        }
+        else {
+            builder.setContentText(WITH_IN_BORDER);
+        }
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            if (equals(location)) {
+                builder.setContentText(AT_BORDER);
+            } else if (out_of_border(location)) {
+                builder.setContentText(OUT_OF_BORDER);
+            }
+            else {
+                builder.setContentText(WITH_IN_BORDER);
+            }
+        }
+        else
+        {
+            builder.setContentText(CONNECTING);
+        }
+
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        builder.setContentText(TURN_ON_LOCATION);
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
     }
 }
